679f34cc774fceb3ea68727630543d12332e33b0
Delapouite <delapouite@gmail.com>
2016-12-03 19:22:59 +0100

    feat: add key-details on hover

--- js/index.js
+++ js/index.js
@@ -26,3 +26,16 @@ document.addEventListener('DOMContentLoaded', () => {
     ya.arrow(Object.assign({ arrowStyles, textStyles }, arrow)).render())
 })
 
+// keys
+
+document.addEventListener('DOMContentLoaded', () => {
+  const kd = document.getElementById('key-details')
+  document.getElementById('keyboard').addEventListener('mouseenter', ({ target }) => {
+    if (target.tagName !== 'A') return false
+
+    const dl = document.querySelector(`dl[data-key=${target.href.split('#')[1]}]`)
+    if (!dl) return false
+
+    kd.innerHTML = dl.innerHTML
+  }, true)
+})
