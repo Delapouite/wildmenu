c47547f634734e0aa4d8ea25493919ba88ed8985
Delapouite <delapouite@gmail.com>
2016-12-04 12:02:42 +0100

    fix(make): ensure correct dir before sediting

--- html/make.sh
+++ html/make.sh
@@ -1,3 +1,4 @@
+cd $(dirname $0)
 # on all lines containing  an include tag, grab the value of the src attribute,
 # give this path to cat and the **e**valuate GNU sed command will feed the output
 sed '/include/ s/.*<include src="\([^"]\+\)">$/cat \1/ e' index.html > ../index.html
